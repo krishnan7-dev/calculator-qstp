69546ca1c7312b5bae0e388e8d7683b878f7853b
Adarsh <agkrishnan2003@gmail.com>
2022-07-26 01:06:44 +0530

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.{html, js}"],
+  content: ["./public/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'violet': '#0064FF',
+        'indigo': '#004CFF',
+        'blue': '#0000FF',
+        'purple': '#A700CC',
+        'pink': '#D9008F',
+        'red': '#EC0051'
+      }
+    },
   },
   plugins: [],
 }
